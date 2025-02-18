d95578d3ceb9b1155d1515117d503f32543a6596
reality-farouqk <mailafiyaumarsulaiman@gmail.com>
2025-02-18 15:37:53 +0100

    removed aboutme test file

--- app/components/AboutMe.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import AboutMe from './AboutMe';
-
-test('renders AboutMe component', () => {
-    render(<AboutMe />);
-    const linkElement = screen.getByText(/about me/i);
-    expect(linkElement).toBeInTheDocument();
-});
