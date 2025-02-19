fe97b1fd053a7346818bd95517231b183efbe362
reality-farouqk <mailafiyaumarsulaiman@gmail.com>
2025-02-19 09:50:22 +0100

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import localFont from "next/font/local";
 import ClientRouter from "./components/ClientRouter";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react"
 
 const NeueMachina = localFont({
   src: [
@@ -40,6 +41,7 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth">
       <body className={`${NeueMachina.className} min-h-screen px-5 md:px-12`}>
         <SpeedInsights />
+        <Analytics/>
         <ClientRouter>
           <main className="main max-w-screen-xl mx-auto my-5 lg:my-6 border-x border-gray-400">{children}</main>
         </ClientRouter>
